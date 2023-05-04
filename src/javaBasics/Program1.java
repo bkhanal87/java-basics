4bc12b892fb3a10bdbbc30ca67207f8c50cf2d06
bkhanal87@gmail.com <bkhanal87@gmail.com>
2023-05-04 19:58:07 -0400

    static value during execution

---
+++ src/javaBasics/Program1.java
@@ -0,0 +1,15 @@
+// static value during execution
+
+
+package javaBasics;
+
+public class Program1 {
+
+	public static void main(String[] args) {
+		int i = 0;
+		do {
+			System.out.println(i);
+			i++;
+		} while(i < 10);
+	}
+}
