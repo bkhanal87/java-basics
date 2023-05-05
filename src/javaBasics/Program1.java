b3e75684b1b16d125d7e8ac2bdcaafbf34080ed2
bkhanal87@gmail.com <bkhanal87@gmail.com>
2023-05-04 20:13:39 -0400

    Arrays in Java

--- src/javaBasics/Program1.java
+++ src/javaBasics/Program1.java
@@ -6,10 +6,17 @@ package javaBasics;
 public class Program1 {
 
 	public static void main(String[] args) {
-		int i = 0;
-		do {
-			System.out.println(i);
-			i++;
-		} while(i < 10);
+		int[] arr = new int[5];
+		arr[0] = 5;
+		arr[1] = 10;
+		arr[2] = 15;
+		arr[3] = 25;
+		arr[4] = 60;
+		
+		System.out.println(arr[0]);
+		System.out.println(arr[1]);
+		System.out.println(arr[2]);
+		System.out.println(arr[3]);
+		System.out.println(arr[4]);		
 	}
 }
