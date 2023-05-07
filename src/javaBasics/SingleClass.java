eab0223d8cc0cda2a66242ddffd88d9462110cfd
bkhanal87@gmail.com <bkhanal87@gmail.com>
2023-05-07 18:17:49 -0400

    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

--- src/javaBasics/SingleClass.java
+++ src/javaBasics/SingleClass.java
@@ -4,7 +4,6 @@ public class SingleClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World!");
+		System.out.println("I am a single class");
 	}
-
 }
